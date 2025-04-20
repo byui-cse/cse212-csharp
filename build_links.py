65da37f5c7ece7a5603096820369314f3ede29a0
Aaron Kay <kaychoro@gmail.com>
2025-04-19 22:35:34 -0600

    Update build_links.py

--- build_links.py
+++ build_links.py
@@ -1,4 +1,25 @@
-﻿links = [
+﻿"""
+1. delete all the assignments from last semester
+2. remove the student roster
+
+Use github cli gh to execute stuff like this:
+gh auth status
+gh repo list -L 100 byui-cse212 | awk '{print $1}'
+Then I drop that in excel to run the delete repo commands like:
+gh repo delete byui-cse212/byui-cse212-classroom-w25-final-project-alt-final-project-alt --yes
+
+recreate all the assignments from prove01-prove10, teach01-teach05, final-project in gh classroom
+
+using excel I did something like this per row:
+1	byui-cse212/prove-01-alt	s25-prove-01		lesson01	prove	https://classroom.github.com/a/knUcBiMV
+to generate the links array down below
+
+run this python script to update the links files (alts are for previous years)
+
+add the student roster back in (student-000, student-001, ..., student-300)
+"""
+
+links = [
     ("lesson01", "prove", "https://classroom.github.com/a/knUcBiMV", True, True),
     ("lesson02", "prove", "https://classroom.github.com/a/PM1Z35wZ", True, True),
     ("lesson04", "prove", "https://classroom.github.com/a/E0F1t5rw", True, True),
