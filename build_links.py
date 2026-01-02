7c13160adb1ec8588a6a3ec63c64adc36fcc3e32
Aaron Kay <kaychoro@gmail.com>
2026-01-02 16:57:24 -0700

    Updated links for W26

--- build_links.py
+++ build_links.py
@@ -8,7 +8,7 @@
     Use github cli gh to execute stuff like this:
     gh auth status
     gh auth refresh -h github.com -s delete_repo
-    gh repo list -L 100 byui-cse-212 | awk '{print $1}'
+    gh repo list -L 100 byui-cse-212 | grep master | awk '{print $1}'
 
     Then I drop that in Excel to run the delete repo commands like:
     (assuming it's in column A, I put this in B1:
@@ -19,13 +19,13 @@
 3.  Recreate all the assignments from prove01-prove10, teach01-teach05, final-project in gh classroom
     - Note that prove-03 and teach-03 do not have a repository - skip those
     - Assignment names should be f25-prove-01 (for Fall 2025, prove 1 assignment)
-    - Template repo should be byui-cse-212/prove-01-alt (until the alt gets removed)
+    - Template repo should be byui-cse-212/prove-01
     - take the link (https://classroom.github.com/a/knUcBiMV) and put it in the python code below
 
 3.  Recreate all the assignments for the final-project in gh classroom
     - Note that prove-03 and teach-03 do not have a repository - skip those
     - Assignment names should be f25-prove-01 (for Fall 2025, prove 1 assignment)
-    - Template repo should be byui-cse-212/prove-01-alt (until the alt gets removed)
+    - Template repo should be byui-cse-212/prove-01
     - take the link (https://classroom.github.com/a/knUcBiMV) and put it in the python code below
 
 4.  Run this python script to update the links so that canvas points to this new semester's
@@ -36,19 +36,20 @@
 """
 
 links = [
-    ("lesson01", "prove", "https://classroom.github.com/a/zew8343a", True, True),
-    ("lesson02", "prove", "https://classroom.github.com/a/bcMFBKVd", True, True),
-    ("lesson04", "prove", "https://classroom.github.com/a/5ddCZjKA", True, True),
-    ("lesson05", "prove", "https://classroom.github.com/a/x_LM7D3V", True, True),
-    ("lesson06", "prove", "https://classroom.github.com/a/CNvHho1I", True, True),
-    ("lesson07", "prove", "https://classroom.github.com/a/7-wdhRSR", True, True),
-    ("lesson08", "prove", "https://classroom.github.com/a/-bHgia0S", True, True),
-    ("lesson09", "prove", "https://classroom.github.com/a/-9nfB1Xm", True, True),
-    ("lesson10", "prove", "https://classroom.github.com/a/igg5eSYR", True, False),
-    ("lesson01", "teach", "https://classroom.github.com/a/pnO22Paq", True, True),
-    ("lesson02", "teach", "https://classroom.github.com/a/PERK7G0g", True, True),
-    ("lesson04", "teach", "https://classroom.github.com/a/XALMmsxW", True, True),
-    ("lesson05", "teach", "https://classroom.github.com/a/L7HIj3-5", True, True),
+    ("lesson01", "prove", "https://classroom.github.com/a/ImjcAjh8", True, True),
+    ("lesson02", "prove", "https://classroom.github.com/a/CjacNbNZ", True, True),
+    ("lesson04", "prove", "https://classroom.github.com/a/9qRS5mSi", True, True),
+    ("lesson05", "prove", "https://classroom.github.com/a/asMoextS", True, True),
+    ("lesson06", "prove", "https://classroom.github.com/a/PDhSGue_", True, True),
+    ("lesson07", "prove", "https://classroom.github.com/a/9fpUobY3", True, True),
+    ("lesson08", "prove", "https://classroom.github.com/a/eTgEpZBc", True, True),
+    ("lesson09", "prove", "https://classroom.github.com/a/T7Jm4i7j", True, True),
+    ("lesson10", "prove", "https://classroom.github.com/a/9hz4sHYc", True, False),
+    ("lesson11", "prove", "https://classroom.github.com/a/r3GD7TQX", True, False), # final project
+    ("lesson01", "teach", "https://classroom.github.com/a/X2B_3Oc7", True, True),
+    ("lesson02", "teach", "https://classroom.github.com/a/12qtq7Wc", True, True),
+    ("lesson04", "teach", "https://classroom.github.com/a/w60mPv3_", True, True),
+    ("lesson05", "teach", "https://classroom.github.com/a/I5sMr-5t", True, True),
 ]
 
 for lesson, type, link, do_regular, do_alt in links:
